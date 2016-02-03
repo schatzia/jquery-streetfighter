922534a9d9a3747cb4e4a1a1edf87ad78726a00f
schatzia <schatzia@hotmail.com>
2016-02-03 22:41:13 +0200

    cool audio

--- js/app.js
+++ js/app.js
@@ -28,20 +28,36 @@ $(document).ready(function() {
 })
 .keydown(function(event){
 			if (event.which == 88){
+				playCool();
 				$('.ryu-ready').hide();
 				$('.ryu-still').hide();
 				$('.ryu.ryu-throwing').hide();
 				$('.ryu').css("background", "url('images/ryu-cool.gif')");
 			}
 })
-.keyup(function(){
-			$('.ryu-still').show();			
+.keyup(function(event){
+		if(event.which==88)	{
+			pauseCoolSound();
+			$('.ryu-still').show();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide()			;
 			$('.ryu').css("background", "none");
 			/* $('.ryu').removeAttr("background"); */
+		}
 });
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
+}
+function playCool () {
+	/* $('#cool-sound')[0].volume = 0.5; */
+	$('#cool-sound')[0].load();
+	$('#cool-sound')[0].play();
+}
+function pauseCoolSound() {
+	$('#cool-sound')[0].pause();
+	$('#cool-sound')[0].load();
+	/* $('#cool-sound')[0].currentTime = 0; */
 }
